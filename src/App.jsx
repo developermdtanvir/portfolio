76025d07542ef9c1ee3686304b7560119998ff74
Tanvir <tanvir@Linux.Linux>
2023-04-25 15:06:54 +0300

    i am added github file

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Mutable from './components/Mutable'
+
+function App() {
+  return (
+    <div>
+      <Mutable />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Mutable.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+
+function Mutable(params) {
+    const [title, setTitle] = useState({
+        firstTitle: "Java",
+        lastTitle: "Script"
+    });
+
+    const changeTitle = () => {
+        const cloneTitle = {
+            ...title,
+        }
+        cloneTitle.firstTitle = "React";
+        cloneTitle.lastTitle = "JS";
+        setTitle(cloneTitle)
+    }
+    return (
+        <div>
+            <h1>{title.firstTitle + " " + title.lastTitle}</h1>
+            <button onClick={!changeTitle}>changeTitle</button>
+        </div>
+    )
+
+}
+
+
+export default Mutable;
