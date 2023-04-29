246aeee4b57b80ffcebbe894d33c4f3562032921
Tanvir <tanvir@Linux.Linux>
2023-04-29 07:19:45 +0300

    my portfolio website is ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import { Suspense } from 'react'
 import './App.css'
-import Mutable from './components/Mutable'
-import Quote from './components/swr/Quote'
 
 function App() {
   return (
     <div>
-      <Mutable />
-      <Suspense
-        fallback={<h1>Loading.........</h1>}
-      ><Quote /></Suspense>
+      <h1>Hello World</h1>
     </div>
   )
 }
