4e8cbf9e4ff63fa7df803bb952f702906cee78c6
mdtanvirgit <freelencertanvirhossain@gmail.com>
2023-05-17 15:16:16 +0600

    finished website landing page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,12 +7,25 @@ function Footer() {
                     <h1 className="">FIND ME ON</h1>
                     <p className=" fs-2">Feel free to <span style={{ color: "#f9004d" }}>connect</span> with me</p>
                     <div>
-                        <span className='fs-3 mx-2'><AiFillGithub /></span>
-                        <span className='fs-3 mx-2'><AiOutlineFacebook /></span>
-                        <span className='fs-3 mx-2'><AiFillLinkedin /></span>
+                        <span className='fs-3 mx-2 btn btn-outline-primary rounded-circle'><AiFillGithub className='' /></span>
+                        <span className='fs-3 mx-2 btn btn-outline-primary rounded-circle'><AiOutlineFacebook /></span>
+                        <span className='fs-3 mx-2 btn btn-outline-primary rounded-circle'><AiFillLinkedin /></span>
                     </div>
                 </div>
             </div>
+            <footer className=' mt-5 row text-light d-flex align-items-center justify-content-between text-center'>
+                <div className='col-md-4'>
+                    <p>Designed and Developed by Mojahidul Islam</p>
+                </div>
+                <div className='col-md-4'>
+                    <p>Copyright © 2023 MD.</p>
+                </div>
+                <div className='col-md-4'>
+                    <AiFillGithub className=' mx-2' />
+                    <AiOutlineFacebook className=' mx-2' />
+                    <AiFillLinkedin className=' mx-2' />
+                </div>
+            </footer>
         </div>
     )
 };
